60fb07d3fa203fc664360fa186a191af3220e772
pathtara <67682370+pathtara@users.noreply.github.com>
2020-07-02 13:49:24 +0700

    Add files via upload

---
+++ Exercise4_Pathtara_T.py
@@ -0,0 +1,38 @@
+"""
+"หากผู้เรียนกำลังศึกษาอยู่ในชั้นปีที่ 2 มหาวิทยาลัยชื่อดังย่านกลางเมืองที่ตอนนี้กำลังสอบเก็บคะแนนกันอย่างสนุกสนาน แล้ววันนึงได้รับมอบหมายจากอาจารย์ที่ปรึกษาให้พัฒนาโปรแกรมให้กับทางคณะ โดย ระบบดังกล่าวจะมีการเก็บคะแนนรายบุคคลของผู้เรียนแต่ละท่าน ของนักศึกษาชั้นปีที่ 2 เทอม 1" โดยมีรายชื่อวิชาดังนี้
+
+Foundation English
+General Business
+Introduction to Computer Systems
+Computer Programming
+โดยให้ผู้เรียนพัฒนาโปรแกรมโดยสร้างตัวแปรสำหรับเก็บคะแนนผู้เรียนในรายวิชาดังกล่าว โดยคะแนนจะสามารถเป็นตัวเลขทศนิยมได้ และ เมื่อได้ตัวแปรมาแล้วให้ทำการแสดงผลออกมาให้ผู้ใช้งานได้เห็นในรูปแบบ
+
+--- Your Score ---
+
+Foundation English : คะแนนที่ได้
+
+General Business : คะแนนที่ได้
+
+Introduction to Computer Systems : คะแนนที่ได้
+
+Computer Programming : คะแนนที่ได้
+ 
+*การตั้งชื่อตัวแปรควรสื่อความหมาย ผู้อ่าน หรือ นักพัฒนาโปรแกรมท่านอื่นสามารถอ่านเข้าใจได้
+"""
+
+### Variable Setup ###
+found_eng = 87.32
+general_biz = 82.43
+intro_com = 93.78
+com_programming = 79.02
+
+### Result ###
+print("               ——— Your Score ———")
+print(" ")
+print("Foundation English               :", found_eng, "/ 100")
+print("General Business                 :", general_biz, "/ 100")
+print("Introduction to Computer System  :", intro_com,"/ 100")
+print("Computer Programming             :", com_programming, "/ 100")
+print("")
+print("           ___________________________")
+
