5a8d9f953f8037b5521f47dcbeafcd4f9ae79872
pathtara <67682370+pathtara@users.noreply.github.com>
2020-07-09 14:34:51 +0700

    Add files via upload
    Lecture50

---
+++ Lecture50_Pathtara_T.py
@@ -0,0 +1,13 @@
+def addNumber(x, y):
+      print(x+y)
+def minusNumber(x, y):
+      print(x-y)
+def multiplyNumber(x, y):
+      print(x*y)
+def divideNumber(x, y):
+      print(x/y)            
+                  
+addNumber(20, 5)
+minusNumber(20, 5)      
+multiplyNumber(20, 5)
+divideNumber(20, 5)
