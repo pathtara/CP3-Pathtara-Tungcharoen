c54674e0d6b5f4ad2a1e128f7edd7d8d4ee6c99b
pathtara <pathtara.t@gmail.com>
2020-08-06 20:28:43 +0700

    add

--- Autopy/Final_Project.py
+++ Autopy/Final_Project.py
@@ -3,18 +3,22 @@ import pandas as pd
 from pandas_datareader import data
 import seaborn as sns
 import matplotlib.pyplot as plt
+import pandas_datareader.yahoo.daily
 #matplotlib inline
 #config InlineBackend.figure_format='retina'
 
+a = [1,2,3]
+b = [7,8,9]
 
-df = data.DataReader('kbank.bk', data_source='yahoo', start='2017-01-01')
-x = sns.lineplot(x=df.index, y='Adj Close', data=df)
+df = data.DataReader('PTT', data_source='yahoo', start='2017-01-01')
+df2=data.NaverDailyReader(symbols='kbank.bk',start='2017-01-01')
 
-print(x)
+#x = sns.lineplot(x=df.index, y='Adj Close', data=df)
+
+print(df.head(2))
 
 
-
-
+print(df2)
 
 class main_windows:
     def register():
--- Autopy/test.py
+++ Autopy/test.py
@@ -1,5 +1,4 @@
-import autopy as at
+from googlefinance import getQuotes
+import json
+json.dumps(getQuotes('PPT'), indent=2)
 
-at.mouse.move(1,1)
-at.mouse.smooth_move(300, 500)
-at.mouse.click()
